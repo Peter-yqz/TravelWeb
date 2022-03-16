ceb492f0cc835df0551e5d85ddc72ada3824fa0e
张金毅 <zhangjinyi@192.168.50.173>
2022-03-16 14:52:49 +0800

    add map

--- src/components/Page3/Page3.vue
+++ src/components/Page3/Page3.vue
@@ -1,33 +1,134 @@
 <template>
-  <baidu-map class="map" center="中国" :zoom="5":scroll-wheel-zoom="true">
-	  <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
-	  <bml-heatmap :data="data" :max="1000" :radius="10">
-  </bml-heatmap>
-  </baidu-map>
+  <div>
+    <div class="picker">
+      <!-- <div style="flex: 1">选择时间：</div> -->
+      <timeLine
+        :timeLineList="timeLineList"
+        @activeChange="changeDate"
+      ></timeLine>
+    </div>
+    <baidu-map class="map" center="中国" :zoom="5" :scroll-wheel-zoom="true">
+      <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+      <bml-heatmap :data="mapData" :max="1000" :radius="10"> </bml-heatmap>
+    </baidu-map>
+  </div>
 </template>
 
 <script>
-import {BmlHeatmap} from 'vue-baidu-map'
-
+import { BmlHeatmap } from "vue-baidu-map";
+import timeLine from "./timeLine.vue";
 export default {
   components: {
-    BmlHeatmap
+    BmlHeatmap,
+    timeLine,
   },
-  data () {
+  data() {
     return {
-      data: [
-        {lng: 116.418261, lat: 39.921984, count: 900},
-        {lng: 118.423332, lat: 38.916532, count: 810},
-        {lng: 121.7192, lat: 31.309853, count: 999}
+      mapData: [
+        { lng: 116.418261, lat: 39.921984, count: 900 },
+        { lng: 118.423332, lat: 38.916532, count: 810 },
+        { lng: 121.7192, lat: 31.309853, count: 999 },
         // ...此处添加更多的数据集
-      ]
-    }
-  }
-}
+      ],
+      originalData: [],
+      timeLineList: [
+        {
+          timestamp: "2013-10-6",
+          info: "2013-10-6",
+        },
+        {
+          timestamp: "2014-10-6",
+          info: "2014-10-6",
+        },
+        {
+          timestamp: "2015-10-6",
+          info: "2015-10-6",
+        },
+        {
+          timestamp: "2016-10-6",
+          info: "2016-10-6",
+        },
+        {
+          timestamp: "2017-10-6",
+          info: "2017-10-6",
+        },
+        {
+          timestamp: "2018-10-6",
+          info: "2018-10-6",
+        },
+        {
+          timestamp: "2019-10-6",
+          info: "2019-10-6",
+        },
+        {
+          timestamp: "2020-10-6",
+          info: "2020-10-6",
+        },
+      ],
+    };
+  },
+  methods: {
+    changeAction(e) {
+      this.startTime = e.startTime;
+      this.endTime = e.endTime;
+    },
+    changeDate(idx) {
+      console.log("父亲改变了： ", idx);
+      this.mapData = [];
+      let correctTime = this.originalData.filter((val) => {
+        return val.date == idx;
+      });
+      console.log(correctTime);
+      correctTime.map((val) => {
+        this.mapData.push({
+          lng: val.lng,
+          lat: val.lat,
+          count: val.num,
+        });
+      });
+    },
+  },
+  mounted: function () {
+    this.$axios({
+      headers: { "content-Type": "application/json;charset=utf-8" },
+      method: "get",
+      url: "map/getData",
+    }).then((res) => {
+      console.log(res);
+      this.originalData = res.data.msg;
+    });
+  },
+  watch: {
+    originalData: function (val) {
+      let timeLable = {};
+      for (let i of val) {
+        // console.log(i);
+        timeLable[i.date] = "";
+      }
+      console.log(timeLable);
+      this.timeLineList = [];
+      const t = Object.keys(timeLable).sort();
+      console.log(t);
+      for (let j of t) {
+        this.timeLineList.push({
+          timestamp: j,
+          info: j,
+        });
+      }
+      console.log(this.timeLineList);
+    },
+  },
+};
 </script>
 <style>
 .map {
   width: 100%;
   height: 100vh;
+}
+.picker {
+  z-index: 100;
+  height: 60px;
+  margin-top: 20px;
+  width: 100vw;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,14 +10,18 @@ import BaiduMap from 'vue-baidu-map'
 // import 'echarts/theme/dark'
 import * as echarts from 'echarts';
 import axios from 'axios'
+import "vue-tlpick"
+import tlpick from "vue-tlpick"
 Vue.prototype.$echarts = echarts
 Vue.prototype.$axios = axios //前后端分离组件
-axios.defaults.baseURL = 'http://8.131.237.67:8010/'  //关键代码
+axios.defaults.baseURL = '/api'  //关键代码
 // Vue.prototype.$echarts = echarts
 // Vue.component('chart', ECharts)
 Vue.use(ElementUI)
 Vue.use(BaiduMap,{ak:'aGwCmimyisgROGpCGqWik4S2C9aQwEpp'})
 Vue.config.productionTip = false
+
+Vue.use(tlpick)
 
 new Vue({
   router,
